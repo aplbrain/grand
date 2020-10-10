4f237f7688b1ca92c2f5fb13cfb98ea849ca4ca8
Jordan Matelsky <j6k4m8@gmail.com>
2020-10-10 12:39:49 -0400

    [DynamoDB] Fast ingest in multiple threads

--- grand/backends/dynamodb.py
+++ grand/backends/dynamodb.py
@@ -438,63 +438,65 @@ class DynamoDBBackend(Backend):
 
         """
         # Ingest edges first:
-        batch_size = 100
+
+        edge_column_names = [
+            c for c in edgelist.columns if c not in [source_column, target_column]
+        ]
+
         tic = time.time()
-        for i in range(0, len(edgelist), batch_size):
-            edges = [
-                {
-                    self._primary_key: f"__{e[source_column]}__{e[target_column]}",
-                    self._edge_source_key: str(e[source_column]),
-                    self._edge_target_key: str(e[target_column]),
-                    **{
-                        k: v
-                        for k, v in e.items()
-                        if k not in [source_column, target_column]
-                    },
-                }
-                for _, e in edgelist[i : i + batch_size].iterrows()
-            ]
-            with self._edge_table.batch_writer() as batch_writer:
+        with self._edge_table.batch_writer() as batch_writer:
 
-                with concurrent.futures.ThreadPoolExecutor(
-                    _N_PARALLEL_REQUESTS
-                ) as executor:
-                    result_futures = list(
-                        map(
-                            lambda x: executor.submit(
-                                lambda: batch_writer.put_item(Item=x)
-                            ),
-                            [edge for edge in edges],
-                        )
+            with concurrent.futures.ThreadPoolExecutor(
+                _N_PARALLEL_REQUESTS
+            ) as executor:
+                result_futures = list(
+                    map(
+                        lambda i: executor.submit(
+                            lambda: batch_writer.put_item(
+                                Item={
+                                    self._primary_key: f"__{edgelist._get_value(i, source_column)}__{edgelist._get_value(i, target_column)}",
+                                    self._edge_source_key: edgelist._get_value(
+                                        i, source_column
+                                    ),
+                                    self._edge_target_key: edgelist._get_value(
+                                        i, target_column
+                                    ),
+                                    **{
+                                        col: edgelist._get_value(i, col)
+                                        for col in edge_column_names
+                                    },
+                                }
+                            )
+                        ),
+                        edgelist.index,
                     )
-                    for future in concurrent.futures.as_completed(result_futures):
-                        future.result()
+                )
+                for future in concurrent.futures.as_completed(result_futures):
+                    future.result()
 
-                # for edge in edges:
-                #     batch_writer.put_item(Item=edge)
+            # for edge in edges:
+            #     batch_writer.put_item(Item=edge)
         edge_toc = time.time() - tic
 
         tic = time.time()
         # Construct a unique set of nodes:
         nodes = edgelist[source_column].append(edgelist[target_column]).unique()
-        for i in range(0, len(nodes), batch_size):
-            with self._node_table.batch_writer() as batch_writer:
-                # TODO: Parallelism here
-                with concurrent.futures.ThreadPoolExecutor(
-                    _N_PARALLEL_REQUESTS
-                ) as executor:
-                    result_futures = list(
-                        map(
-                            lambda x: executor.submit(
-                                lambda: batch_writer.put_item(
-                                    Item={self._primary_key: str(x)}
-                                )
-                            ),
-                            nodes[i : i + batch_size],
-                        )
+        with self._node_table.batch_writer() as batch_writer:
+            with concurrent.futures.ThreadPoolExecutor(
+                _N_PARALLEL_REQUESTS
+            ) as executor:
+                result_futures = list(
+                    map(
+                        lambda x: executor.submit(
+                            lambda: batch_writer.put_item(
+                                Item={self._primary_key: str(x)}
+                            )
+                        ),
+                        nodes,
                     )
-                    for future in concurrent.futures.as_completed(result_futures):
-                        future.result()
+                )
+                for future in concurrent.futures.as_completed(result_futures):
+                    future.result()
 
         return {
             "node_count": len(nodes),
