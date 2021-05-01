fbb770ad8783e1b5984eeaf42722ec5a16835782
Jordan Matelsky <j6k4m8@gmail.com>
2021-04-30 22:00:01 -0400

    Add version to package

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="grand-graph",
-    version="0.1.0",
+    version="0.2.0",
     author="Jordan Matelsky",
     author_email="opensource@matelsky.com",
     description="Graph database wrapper for non-graph datastores",
@@ -15,13 +15,13 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/aplbrain/grand",
     packages=setuptools.find_packages(),
-    install_requires = [
-        'boto3',
-        'networkx==2.4',
-        'numpy==1.19.1',
-        'pandas==1.1.0',
-        'SQLAlchemy==1.3.18'
-        ],
+    install_requires=[
+        "boto3",
+        "networkx==2.4",
+        "numpy==1.19.1",
+        "pandas==1.1.0",
+        "SQLAlchemy==1.3.18",
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
