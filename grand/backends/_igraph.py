b7fdbbb9d74f4a7e77390f5f956a2c47fec00e56
Jordan Matelsky <j6k4m8@gmail.com>
2026-03-20 10:24:41 -0400

    perf: add named node management in IGraphBackend

--- grand/backends/_igraph.py
+++ grand/backends/_igraph.py
@@ -1,6 +1,6 @@
 from typing import Hashable, Collection
 
-from igraph import Graph, InternalError
+from igraph import Graph
 import pandas as pd
 
 from .backend import Backend
@@ -35,6 +35,13 @@ class IGraphBackend(Backend):
         """
         self._directed = directed
         self._ig = Graph(directed=self._directed)
+        self._node_ids_by_name = {}
+        self._edge_ids_by_key = {}
+
+    def _edge_key(self, u: Hashable, v: Hashable):
+        if self._directed:
+            return (u, v)
+        return frozenset((u, v))
 
     def ingest_from_edgelist_dataframe(
         self, edgelist: pd.DataFrame, source_column: str, target_column: str
@@ -70,12 +77,15 @@ class IGraphBackend(Backend):
             Hashable: The ID of this node, as inserted
 
         """
+        metadata = metadata or {}
+
         if self.has_node(node_name):
             # Update metadata
-            m = self._ig.vs.find(name=node_name)
-            m.update_attributes(metadata)
+            self._ig.vs[self._node_ids_by_name[node_name]].update_attributes(metadata)
             return node_name
+
         self._ig.add_vertex(name=node_name, **metadata)
+        self._node_ids_by_name[node_name] = self._ig.vcount() - 1
         return node_name
 
     def get_node_by_id(self, node_name: Hashable):
@@ -90,7 +100,7 @@ class IGraphBackend(Backend):
 
         """
         return _remove_name_from_attributes(
-            self._ig.vs.find(name=node_name).attributes()
+            self._ig.vs[self._node_ids_by_name[node_name]].attributes()
         )
 
     def all_nodes_as_iterable(self, include_metadata: bool = False) -> Collection:
@@ -122,11 +132,7 @@ class IGraphBackend(Backend):
         Returns:
             bool: True if the node exists
         """
-        try:
-            self._ig.vs.find(name=u)
-            return True
-        except:
-            return False
+        return u in self._node_ids_by_name
 
     def add_edge(self, u: Hashable, v: Hashable, metadata: dict):
         """
@@ -144,16 +150,24 @@ class IGraphBackend(Backend):
             Hashable: The edge ID, as inserted.
 
         """
-        if self.has_edge(u, v):
-            # Update metadata
-            e = self._ig.get_eid(u, v)
-            self._ig.es[e].update_attributes(metadata)
+        metadata = metadata or {}
+        edge_key = self._edge_key(u, v)
+
+        if edge_key in self._edge_ids_by_key:
+            self._ig.es[self._edge_ids_by_key[edge_key]].update_attributes(metadata)
             return
         if not self.has_node(u):
             self.add_node(u, {})
         if not self.has_node(v):
             self.add_node(v, {})
-        return self._ig.add_edge(source=u, target=v, **metadata)
+
+        new_edge = self._ig.add_edge(
+            source=self._node_ids_by_name[u],
+            target=self._node_ids_by_name[v],
+            **metadata,
+        )
+        self._edge_ids_by_key[edge_key] = self._ig.ecount() - 1
+        return new_edge
 
     def all_edges_as_iterable(self, include_metadata: bool = False) -> Collection:
         """
@@ -174,13 +188,7 @@ class IGraphBackend(Backend):
                 yield e.source_vertex["name"], e.target_vertex["name"]
 
     def has_edge(self, u, v):
-        try:
-            self._ig.get_eid(u, v)
-            return True
-        except (InternalError, ValueError):
-            # InternalError means no such edge
-            # ValueError means one vertex doesn't exist
-            return False
+        return self._edge_key(u, v) in self._edge_ids_by_key
 
     def get_edge_by_id(self, u: Hashable, v: Hashable):
         """
@@ -194,10 +202,10 @@ class IGraphBackend(Backend):
             dict: Metadata associated with this edge
 
         """
-        try:
-            return self._ig.es[self._ig.get_eid(u, v)].attributes()
-        except InternalError:
+        edge_id = self._edge_ids_by_key.get(self._edge_key(u, v))
+        if edge_id is None:
             raise IndexError(f"The edge ({u}, {v}) is not in the graph.")
+        return self._ig.es[edge_id].attributes()
 
     def get_node_successors(
         self, u: Hashable, include_metadata: bool = False
@@ -217,13 +225,15 @@ class IGraphBackend(Backend):
             Generator
 
         """
+        u_id = self._node_ids_by_name[u]
+
         if include_metadata:
             return {
-                self._ig.vs[s]["name"]: self.get_edge_by_id(u, s)
-                for s in self._ig.successors(u)
+                self._ig.vs[s]["name"]: self.get_edge_by_id(u, self._ig.vs[s]["name"])
+                for s in self._ig.successors(u_id)
             }
         else:
-            return iter([self._ig.vs[s]["name"] for s in self._ig.successors(u)])
+            return iter([self._ig.vs[s]["name"] for s in self._ig.successors(u_id)])
 
     def get_node_predecessors(
         self, u: Hashable, include_metadata: bool = False
@@ -238,13 +248,15 @@ class IGraphBackend(Backend):
             Generator
 
         """
+        u_id = self._node_ids_by_name[u]
+
         if include_metadata:
             return {
-                self._ig.vs[s]["name"]: self.get_edge_by_id(s, u)
-                for s in self._ig.predecessors(u)
+                self._ig.vs[s]["name"]: self.get_edge_by_id(self._ig.vs[s]["name"], u)
+                for s in self._ig.predecessors(u_id)
             }
         else:
-            return iter([self._ig.vs[s]["name"] for s in self._ig.predecessors(u)])
+            return iter([self._ig.vs[s]["name"] for s in self._ig.predecessors(u_id)])
 
     def get_node_count(self) -> int:
         """
